5d1aaad9d289341ec80e38102874add5584fe00d
vuqar9494 <82055193+vuqar9494@users.noreply.github.com>
2023-10-09 17:56:13 +0400

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -203,7 +203,7 @@ crossIcon = function(cell, formatterParams){ //plain text value
     let st0=this.AllData.filter(x=>x.status==0).length
     let st1=this.AllData.filter(x=>x.status==1).length
     let st2=this.AllData.filter(x=>x.status==2).length
-    this.pieChartLabels = [  `Status 0 (${Math.round((100/all)*st0)})` ,  `Status 1 (${Math.round((100/all)*st1)})` , `Status 2 (${Math.round((100/all)*st2)})` ];
+    this.pieChartLabels = [  `Status 0 (${Math.round((100/all)*st0)}%)` ,  `Status 1 (${Math.round((100/all)*st1)}%)` , `Status 2 (${Math.round((100/all)*st2)}%)` ];
      this.pieChartDatasets = [ {
       data: [ 
         st0 ,
